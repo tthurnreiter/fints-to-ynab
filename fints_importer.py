d007d4072f95eeb7414f58cfd39dc9391c4825ee
Tobias Thurnreiter <72652575+tthurnreiter@users.noreply.github.com>
2021-02-21 00:22:03 +0100

    Workaround for ING
    See https://github.com/raphaelm/python-fints/issues/108https://github.com/raphaelm/python-fints/issues/108

--- fints_importer.py
+++ fints_importer.py
@@ -15,6 +15,7 @@ def is_cash_withdrawl(transaction):
     return False
 
 def transform_fints_transaction(transaction, parse_paypal=False):
+    print(transaction)
     payee = transaction['applicant_name'] or ''
     memo = transaction['purpose'] or ''
 
@@ -38,16 +39,16 @@ def get_transactions(bank_config):
         product_id='33D93BB1B017D422A87837C01'
     )
 
-    minimal_interactive_cli_bootstrap(f)
-
-    with f:
-        # Since PSD2, a TAN might be needed for dialog initialization. Let's check if there is one required
-        if f.init_tan_response:
-            print("A TAN is required", f.init_tan_response.challenge)
-            tan = input('Please enter TAN:')
-            f.send_tan(f.init_tan_response, tan)
-        # Fetch accounts
-        accounts = f.get_sepa_accounts()
+#    minimal_interactive_cli_bootstrap(f)
+#
+#    with f:
+#        # Since PSD2, a TAN might be needed for dialog initialization. Let's check if there is one required
+#        if f.init_tan_response:
+#            print("A TAN is required", f.init_tan_response.challenge)
+#            tan = input('Please enter TAN:')
+#            f.send_tan(f.init_tan_response, tan)
+#        # Fetch accounts
+    accounts = f.get_sepa_accounts()
 
     # get transactions
     account = next(filter(lambda a: a.iban == bank_config.iban, accounts), None)
